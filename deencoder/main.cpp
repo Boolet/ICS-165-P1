3470f3180e18c89b10096d9fc61c711357f73bb8
dptran11111 <danielpt@uci.edu>
2018-06-06 02:06:49 -0700

    Add files via upload

--- deencoder/main.cpp
+++ deencoder/main.cpp
@@ -35,7 +35,7 @@ int main(int argc, char** argv)
 {
 	ifstream inFile;
 	char c;
-	int index = 1, wIndex = 0;
+	int index = 1, wIndex = 0, leftover = 0;
 	int bitLength = 0; //Index of NLS
 	int currentType = 0; //Checks whether we are looking for the bits of length, offset, string length, or char
 	bitset<16> bitC, currentBits;
@@ -64,10 +64,12 @@ int main(int argc, char** argv)
 	char window[pow(2,NLS[0])];
 	currentBits |= (currentC << 12 >> 16-NLS[1]);
 	bitLength = 4;
+	
 
 	while(inFile.get(c))
 	{
 		currentC = bitset<16>(c);
+		int leftover = 8
 		if(NLS[index]-bitLength > 8)
 		{
 			currentBits |= (currentC << NLS[index]-bitLength-8);
@@ -75,38 +77,41 @@ int main(int argc, char** argv)
 		}
 		else
 		{
-			currentBits |= (currentC >> 8 - (NLS[index] - bitLength));
-			int leftover = 8 - NLS[index] + bitLength;
-			tempNLS[index] = currentBits.to_ulong();
-			currentBits = bitset<16>(0);
-			value = changeIndex(index, tempNLS);
-			if (value == 1)
+			do while(leftover >= NLS[index])
 			{
-				for(int i = 0; i <= tempNLS[1]; i++)
+				currentBits |= (currentC << 16 - leftover >> 16 - (NLS[index] - bitLength));
+				leftover -= NLS[index] - bitLength;
+				tempNLS[index] = currentBits.to_ulong();
+				currentBits = bitset<16>(0);
+				value = changeIndex(index, tempNLS);
+				if (value == 1)
 				{
-					window[wIndex+i] = window[wIndex+i-tempNLS[0]];
-					cout << window[wIndex+1-tempNLS[0]];
-				}
-				tempNLS[0] = 0;
-				tempNLS[1] = 0;
-			}
-			else if (value == 2)
-			{
-				for(int i = 0; i < tempNLS[2]; i++)
-				{
-					currentBits |= (currentC << 16 - leftover >> 8);
-					if(!inFile.get(c))
+					for(int i = 0; i <= tempNLS[1]; i++)
 					{
-						inFile.close();
-						return 0;
+						window[wIndex+i] = window[wIndex+i-tempNLS[0]];
+						cout << window[wIndex+1-tempNLS[0]];
 					}
-					currentC = bitset<16>(c);
-					currentBits |= (currentC >> (8 - leftover));
-					window[wIndex+i] = char(currentBits.to_ulong());
-					currentBits = bitset<16>(0);
+					tempNLS[0] = 0;
+					tempNLS[1] = 0;
 				}
-				tempNLS[2] = 0;
-				tempNLS[1] = 0;
+				else if (value == 2)
+				{
+					for(int i = 0; i < tempNLS[2]; i++)
+					{
+						currentBits |= (currentC << 16 - leftover >> 8);
+						if(!inFile.get(c))
+						{
+							inFile.close();
+							return 0;
+						}
+						currentC = bitset<16>(c);
+						currentBits |= (currentC >> (8 - leftover));
+						window[wIndex+i] = char(currentBits.to_ulong());
+						currentBits = bitset<16>(0);
+					}
+					tempNLS[2] = 0;
+					tempNLS[1] = 0;
+				}
 			}
 			currentBits |= (currentC << 16 - leftover >> 16-NLS[index]);
 			bitLength = leftover;
